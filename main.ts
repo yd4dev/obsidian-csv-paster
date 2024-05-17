41289576564cd76a32cb93205acc37e12f8ae7c8
Dennis Yildirim <dennis@yld.im>
2024-05-17 16:59:46 +0200

    Added functionality

--- main.ts
+++ main.ts
@@ -1,134 +1,193 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+    App,
+    Editor,
+    MarkdownView,
+    Modal,
+    Notice,
+    Plugin,
+    PluginSettingTab,
+    Setting,
+} from "obsidian";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface CsvPasterSettings {
+    mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: CsvPasterSettings = {
+    mySetting: "default",
+};
+
+export default class CsvPasterPlugin extends Plugin {
+    settings: CsvPasterSettings;
+
+    async onload() {
+        await this.loadSettings();
+
+        // This creates an icon in the left ribbon.
+        const ribbonIconEl = this.addRibbonIcon(
+            "dice",
+            "Sample Plugin",
+            (evt: MouseEvent) => {
+                // Called when the user clicks the icon.
+                new Notice("This is a notice!");
+            },
+        );
+        // Perform additional things with the ribbon
+        ribbonIconEl.addClass("my-plugin-ribbon-class");
+
+        // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+        const statusBarItemEl = this.addStatusBarItem();
+        statusBarItemEl.setText("Status Bar Text");
+
+        // This adds a simple command that can be triggered anywhere
+        //  this.addCommand({
+        //    id: "open-sample-modal-simple",
+        //    name: "Open sample modal (simple)",
+        //    callback: () => {
+        //        new PasterModal(this.app).open();
+        //    },
+        //});
+        // This adds an editor command that can perform some operation on the current editor instance
+        //  this.addCommand({
+        //    id: "sample-editor-command",
+        //    name: "Sample editor command",
+        //    editorCallback: (editor: Editor, view: MarkdownView) => {
+        //        console.log(editor.getSelection());
+        //        editor.replaceSelection("Sample Editor Command");
+        //    },
+        //  });
+        // This adds a complex command that can check whether the current state of the app allows execution of the command
+        this.addCommand({
+            id: "paste-csv",
+            name: "Paste CSV into Table",
+            checkCallback: (checking: boolean) => {
+                // Conditions to check
+                const markdownView =
+                    this.app.workspace.getActiveViewOfType(MarkdownView);
+                if (markdownView) {
+                    // If checking is true, we're simply "checking" if the command can be run.
+                    // If checking is false, then we want to actually perform the operation.
+                    if (!checking) {
+                        new PasterModal(this.app, (result) => {
+                            markdownView.editor.getCursor();
+                            markdownView.editor.replaceRange(
+                                csvToTable(result),
+                                markdownView.editor.getCursor(),
+                            );
+                        }).open();
+                    }
+
+                    // This command will only show up in Command Palette when the check function returns true
+                    return true;
+                }
+            },
+        });
+
+        // This adds a settings tab so the user can configure various aspects of the plugin
+        this.addSettingTab(new SettingTab(this.app, this));
+
+        // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+        // Using this function will automatically remove the event listener when this plugin is disabled.
+        this.registerDomEvent(document, "click", (evt: MouseEvent) => {
+            console.log("click", evt);
+        });
+
+        // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+        this.registerInterval(
+            window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000),
+        );
+    }
+
+    onunload() {}
+
+    async loadSettings() {
+        this.settings = Object.assign(
+            {},
+            DEFAULT_SETTINGS,
+            await this.loadData(),
+        );
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class PasterModal extends Modal {
+    result: string;
+    onSubmit: (result: string) => void;
 
-	async onload() {
-		await this.loadSettings();
+    constructor(app: App, onSubmit: (result: string) => void) {
+        super(app);
+        this.onSubmit = onSubmit;
+    }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    onOpen() {
+        const { contentEl } = this;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        contentEl.createEl("h1", { text: "Paste your CVS" });
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        new Setting(contentEl).addTextArea((text) =>
+            text.onChange((value) => {
+                this.result = value;
+            }),
+        );
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        new Setting(contentEl).addButton((btn) =>
+            btn
+                .setButtonText("Submit")
+                .setCta()
+                .onClick(() => {
+                    this.close();
+                    this.onSubmit(this.result);
+                }),
+        );
+    }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    onClose() {
+        let { contentEl } = this;
+        contentEl.empty();
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+function csvToTable(input: string): string {
+    const rows = input.split("\n");
+    let result = "|";
+    const rowCount = rows[0].split(",").length;
+    result +=
+        rows[0].replace(/,/g, "|") + "|\n" + "|---".repeat(rowCount) + "|";
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+    for (let i = 1; i < rows.length - 1; i++) {
+        result += "\n|" + rows[i].replace(/,/g, "|") + "|";
+    }
+    return result;
 }
+class SettingTab extends PluginSettingTab {
+    plugin: CsvPasterPlugin;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+    constructor(app: App, plugin: CsvPasterPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    display(): void {
+        const { containerEl } = this;
 
-	display(): void {
-		const {containerEl} = this;
+        containerEl.empty();
 
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Setting(containerEl)
+            .setName("Setting #1")
+            .setDesc("It's a secret")
+            .addText((text) =>
+                text
+                    .setPlaceholder("Enter your secret")
+                    .setValue(this.plugin.settings.mySetting)
+                    .onChange(async (value) => {
+                        this.plugin.settings.mySetting = value;
+                        await this.plugin.saveSettings();
+                    }),
+            );
+    }
 }
