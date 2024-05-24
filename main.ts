a227cf32ea48673dad06bff72a07d69722e73981
Dennis Yildirim <dennis@yld.im>
2024-05-24 09:19:17 +0200

    Clean Up Code

--- main.ts
+++ main.ts
@@ -1,67 +1,10 @@
-import {
-    App,
-    Editor,
-    MarkdownView,
-    Modal,
-    Notice,
-    Plugin,
-    PluginSettingTab,
-    Setting,
-} from "obsidian";
-
-// Remember to rename these classes and interfaces!
-
-interface CsvPasterSettings {
-    mySetting: string;
-}
-
-const DEFAULT_SETTINGS: CsvPasterSettings = {
-    mySetting: "default",
-};
+import { MarkdownView, Plugin } from "obsidian";
 
 export default class CsvPasterPlugin extends Plugin {
-    settings: CsvPasterSettings;
-
     async onload() {
-        await this.loadSettings();
-
-        // This creates an icon in the left ribbon.
-        const ribbonIconEl = this.addRibbonIcon(
-            "dice",
-            "Sample Plugin",
-            (evt: MouseEvent) => {
-                // Called when the user clicks the icon.
-                new Notice("This is a notice!");
-            },
-        );
-        // Perform additional things with the ribbon
-        ribbonIconEl.addClass("my-plugin-ribbon-class");
-
-        // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-        const statusBarItemEl = this.addStatusBarItem();
-        statusBarItemEl.setText("Status Bar Text");
-
-        // This adds a simple command that can be triggered anywhere
-        //  this.addCommand({
-        //    id: "open-sample-modal-simple",
-        //    name: "Open sample modal (simple)",
-        //    callback: () => {
-        //        new PasterModal(this.app).open();
-        //    },
-        //});
-        // This adds an editor command that can perform some operation on the current editor instance
-        //  this.addCommand({
-        //    id: "sample-editor-command",
-        //    name: "Sample editor command",
-        //    editorCallback: (editor: Editor, view: MarkdownView) => {
-        //        console.log(editor.getSelection());
-        //        editor.replaceSelection("Sample Editor Command");
-        //    },
-        //  });
-        // This adds a complex command that can check whether the current state of the app allows execution of the command
         this.addCommand({
             id: "paste-csv",
-            name: "Paste CSV into Table",
+            name: "Paste CSV from Clipboard into Table",
             checkCallback: (checking: boolean) => {
                 // Conditions to check
                 const markdownView =
@@ -70,23 +13,18 @@ export default class CsvPasterPlugin extends Plugin {
                     // If checking is true, we're simply "checking" if the command can be run.
                     // If checking is false, then we want to actually perform the operation.
                     if (!checking) {
-                        new PasterModal(this.app, (result) => {
-                            markdownView.editor.getCursor();
+                        navigator.clipboard.readText().then((result) => {
                             markdownView.editor.replaceRange(
-                                csvToTable(result),
+                                "\n" + csvToTable(result),
                                 markdownView.editor.getCursor(),
                             );
-                        }).open();
+                        });
                     }
-
                     // This command will only show up in Command Palette when the check function returns true
                     return true;
                 }
             },
         });
-
-        // This adds a settings tab so the user can configure various aspects of the plugin
-        this.addSettingTab(new SettingTab(this.app, this));
 
         // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
         // Using this function will automatically remove the event listener when this plugin is disabled.
@@ -101,55 +39,6 @@ export default class CsvPasterPlugin extends Plugin {
     }
 
     onunload() {}
-
-    async loadSettings() {
-        this.settings = Object.assign(
-            {},
-            DEFAULT_SETTINGS,
-            await this.loadData(),
-        );
-    }
-
-    async saveSettings() {
-        await this.saveData(this.settings);
-    }
-}
-
-class PasterModal extends Modal {
-    result: string;
-    onSubmit: (result: string) => void;
-
-    constructor(app: App, onSubmit: (result: string) => void) {
-        super(app);
-        this.onSubmit = onSubmit;
-    }
-
-    onOpen() {
-        const { contentEl } = this;
-
-        contentEl.createEl("h1", { text: "Paste your CVS" });
-
-        new Setting(contentEl).addTextArea((text) =>
-            text.onChange((value) => {
-                this.result = value;
-            }),
-        );
-
-        new Setting(contentEl).addButton((btn) =>
-            btn
-                .setButtonText("Submit")
-                .setCta()
-                .onClick(() => {
-                    this.close();
-                    this.onSubmit(this.result);
-                }),
-        );
-    }
-
-    onClose() {
-        let { contentEl } = this;
-        contentEl.empty();
-    }
 }
 
 function csvToTable(input: string): string {
@@ -163,31 +52,4 @@ function csvToTable(input: string): string {
         result += "\n|" + rows[i].replace(/,/g, "|") + "|";
     }
     return result;
-}
-class SettingTab extends PluginSettingTab {
-    plugin: CsvPasterPlugin;
-
-    constructor(app: App, plugin: CsvPasterPlugin) {
-        super(app, plugin);
-        this.plugin = plugin;
-    }
-
-    display(): void {
-        const { containerEl } = this;
-
-        containerEl.empty();
-
-        new Setting(containerEl)
-            .setName("Setting #1")
-            .setDesc("It's a secret")
-            .addText((text) =>
-                text
-                    .setPlaceholder("Enter your secret")
-                    .setValue(this.plugin.settings.mySetting)
-                    .onChange(async (value) => {
-                        this.plugin.settings.mySetting = value;
-                        await this.plugin.saveSettings();
-                    }),
-            );
-    }
 }
